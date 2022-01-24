df9babc12c42015cd17e2befcd3cf38f70a5a3b3
Mr_Money <740363495@qq.com>
2022-01-24 17:15:24 +0800

    简单中间件

--- App/Index/index.go
+++ App/Index/index.go
@@ -1,6 +1,7 @@
 package Index
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -15,4 +16,11 @@ func Home1(c *gin.Context) {
 
 func Home2(c *gin.Context) {
 	c.String(http.StatusOK, "Home2 page")
+}
+
+func Middle(c *gin.Context) {
+	req := c.Query("request")
+	fmt.Println("request:", req)
+	// 页面接收
+	c.JSON(200, gin.H{"request": req})
 }
