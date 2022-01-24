9ef830eaf83f410e3f2d12951aea463eddfffeb6
Mr_Money <740363495@qq.com>
2022-01-24 18:31:58 +0800

    配置结构体

--- Config/config.go
+++ Config/config.go
@@ -1,1 +1,30 @@
 package Config
+
+type WebConfig struct {
+	Common interface{}
+	DB     struct {
+		Local struct {
+			Host    string
+			Port    uint16
+			DbName  string
+			User    string
+			Pwd     string
+			Prefix  string
+			Charest string
+			Type    string
+		}
+	}
+	Redis struct {
+		Local struct {
+			Host   string
+			Port   uint16
+			Db     uint8
+			Pwd    string
+			Prefix string
+		}
+	}
+}
+
+func ConfInit() {
+	//https://github.com/BurntSushi/toml
+}
