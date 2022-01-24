53492f0fae3cf69729e9d883378e884284f04dc2
mr_money <740363495@qq.com>
2022-01-24 14:51:19 +0800

    pkg名

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"go-study/router"
+	"go-study/Router"
 )
 
 //
@@ -13,7 +13,7 @@ func main() {
 	// 创建路由引擎
 	r := gin.Default()
 
-	router.Router(r)
+	Router.Router(r)
 
 	// 监听端口，默认在8080
 	// Run(":8000")
