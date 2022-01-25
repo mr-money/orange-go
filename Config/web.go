3044af4c0fd65df5c01cb0c08b4717db2911fd4b
Mr_Money <740363495@qq.com>
2022-01-25 16:41:31 +0800

    toml

--- Config/web.go
+++ Config/web.go
@@ -1,26 +1,22 @@
 package Config
 
 type Web struct {
-	Common interface{}      `toml:"COMMON"`
-	DB     map[string]db    `toml:"DB"`
-	Redis  map[string]redis `toml:"REDIS"`
-}
-
-type db struct {
-	Host    string `toml:"host"`
-	Port    uint16 `toml:"port"`
-	DbName  string `toml:"db_name"`
-	User    string `toml:"user"`
-	Pwd     string `toml:"pwd"`
-	Prefix  string `toml:"prefix"`
-	Charest string `toml:"charest"`
-	Type    string `toml:"type"`
-}
-
-type redis struct {
-	Host   string `toml:"host"`
-	Port   uint16 `toml:"port"`
-	Db     uint8  `toml:"db"`
-	Pwd    string `toml:"pwd"`
-	Prefix string `toml:"prefix"`
+	Common interface{} `toml:"COMMON"`
+	DB     map[string]struct {
+		Host    string `toml:"host"`
+		Port    uint16 `toml:"port"`
+		DbName  string `toml:"db_name"`
+		User    string `toml:"user"`
+		Pwd     string `toml:"pwd"`
+		Prefix  string `toml:"prefix"`
+		Charest string `toml:"charest"`
+		Type    string `toml:"type"`
+	} `toml:"DB"`
+	Redis map[string]struct {
+		Host   string `toml:"host"`
+		Port   uint16 `toml:"port"`
+		Db     uint8  `toml:"db"`
+		Pwd    string `toml:"pwd"`
+		Prefix string `toml:"prefix"`
+	} `toml:"REDIS"`
 }
