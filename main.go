c2932af9761f9fecdc25aa940b3c0c29ebe56b58
Mr_Money <740363495@qq.com>
2022-02-11 15:15:58 +0800

    main config和route的初始化

--- main.go
+++ main.go
@@ -15,19 +15,17 @@ func main() {
 
 	Config.Include(webConfig)
 
-	//初始化配置
-	Config.Init()
-
 	// 加载路由配置
 	Routes.Include(
 		Routes.Web, //默认web路由
 		Routes.Api) //TODO api路由，需要token中间件验证
 
-	//// 初始化路由
-	r := Routes.Init()
-
 	//
 	//// 监听端口，默认在8080
 	// Run(":8000")
-	_ = r.Run()
+	err := Routes.GinEngine.Run()
+	if err != nil {
+		panic(err.Error())
+		return
+	}
 }
