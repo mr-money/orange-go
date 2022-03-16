c272ec7fa9a886ae75d99e1a9526fc1bc167f332
Mr_Money <740363495@qq.com>
2022-03-16 16:39:11 +0800

    model格式化时间

--- Library/time.go
+++ Library/time.go
@@ -1,1 +1,53 @@
 package Library
+
+import (
+	"database/sql/driver"
+	"time"
+)
+
+const TimeFormat = "2006-01-02 15:04:05"
+
+type Time time.Time
+
+func (t *Time) UnmarshalJSON(data []byte) (err error) {
+	// 空值不进行解析
+	if len(data) == 2 {
+		*t = Time(time.Time{})
+		return
+	}
+
+	// 指定解析的格式
+	now, err := time.Parse(`"`+TimeFormat+`"`, string(data))
+	*t = Time(now)
+	return
+}
+
+func (t Time) MarshalJSON() ([]byte, error) {
+	b := make([]byte, 0, len(TimeFormat)+2)
+	b = append(b, '"')
+	b = time.Time(t).AppendFormat(b, TimeFormat)
+	b = append(b, '"')
+	return b, nil
+}
+
+// 写入 mysql 时调用
+func (t Time) Value() (driver.Value, error) {
+	// 0001-01-01 00:00:00 属于空值，遇到空值解析成 null 即可
+	if t.String() == "0001-01-01 00:00:00" {
+		return nil, nil
+	}
+	return []byte(time.Time(t).Format(TimeFormat)), nil
+}
+
+// 检出 mysql 时调用
+func (t *Time) Scan(v interface{}) error {
+	// mysql 内部日期的格式可能是 2006-01-02 15:04:05 +0800 CST 格式，所以检出的时候还需要进行一次格式化
+	tTime, _ := time.Parse("2006-01-02 15:04:05 +0800 CST", v.(time.Time).String())
+	*t = Time(tTime)
+	return nil
+}
+
+// 用于 fmt.Println 和后续验证场景
+func (t Time) String() string {
+	return time.Time(t).Format(TimeFormat)
+}
