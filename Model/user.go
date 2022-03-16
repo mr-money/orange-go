af55e2f7465285ec8a9e5d668d147b889171b955
mr_money <740363495@qq.com>
2022-03-16 16:39:11 +0800

    model格式化时间

--- Model/user.go
+++ Model/user.go
@@ -1,9 +1,9 @@
 package Model
 
 import (
+	"go-study/Library"
 	"go-study/Library/Gorm"
 	"gorm.io/gorm"
-	"time"
 )
 
 //表名
@@ -14,11 +14,11 @@ var tableName = "user"
 // @Description: 表字段结构体
 //
 type User struct {
-	ID        uint64     `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
-	Name      string     `gorm:"column:name;" json:"name"`
-	CreatedAt *time.Time `gorm:"column:created_at;" json:"created_at"`
-	UpdatedAt *time.Time `gorm:"column:updated_at;" json:"updated_at"`
-	DeletedAt *time.Time `gorm:"column:deleted_at;" json:"-"`
+	ID        uint64        `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	Name      string        `gorm:"column:name;" json:"name"`
+	CreatedAt *Library.Time `gorm:"column:created_at;" json:"created_at"`
+	UpdatedAt *Library.Time `gorm:"column:updated_at;" json:"updated_at"`
+	DeletedAt *Library.Time `gorm:"column:deleted_at;" json:"-"`
 }
 
 var UserModel *gorm.DB
