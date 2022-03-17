e03262c6bb420aed3b05216e24ffdc208a9ffd59
Mr_Money <740363495@qq.com>
2022-03-17 16:12:29 +0800

    time方法修改

--- Model/user.go
+++ Model/user.go
@@ -2,8 +2,8 @@ package Model
 
 import (
 	uuid "github.com/satori/go.uuid"
-	"go-study/Library"
 	"go-study/Library/Gorm"
+	"go-study/Library/MyTime"
 	"gorm.io/gorm"
 )
 
@@ -15,12 +15,12 @@ var tableName = "user"
 // @Description: 表字段结构体
 //
 type User struct {
-	ID        uint64        `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
-	Uuid      uuid.UUID     `gorm:"column:uuid;" json:"uuid"`
-	Name      string        `gorm:"column:name;" json:"name"`
-	CreatedAt *Library.Time `gorm:"column:created_at;" json:"created_at"`
-	UpdatedAt *Library.Time `gorm:"column:updated_at;" json:"updated_at"`
-	DeletedAt *Library.Time `gorm:"column:deleted_at;" json:"-"`
+	ID        uint64       `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	Uuid      uuid.UUID    `gorm:"column:uuid;" json:"uuid"`
+	Name      string       `gorm:"column:name;" json:"name"`
+	CreatedAt *MyTime.Time `gorm:"column:created_at;" json:"created_at"`
+	UpdatedAt *MyTime.Time `gorm:"column:updated_at;" json:"updated_at"`
+	DeletedAt *MyTime.Time `gorm:"column:deleted_at;" json:"-"`
 }
 
 // UserModel
