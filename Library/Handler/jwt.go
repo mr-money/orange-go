34d931fbf33b5d68fed2f531ab3f6de5bc02ee70
Mr_Money <740363495@qq.com>
2022-05-17 18:58:57 +0800

    jwt失效时间

--- Library/Handler/jwt.go
+++ Library/Handler/jwt.go
@@ -3,16 +3,17 @@ package Handler
 import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/shockerli/cvt"
+	"go-study/Library/MyTime"
 	"go-study/Model"
 	"time"
 )
 
 type ApiLoginClaims struct {
-	Username string `json:"username"`
+	UserId uint64 `json:"user_id"`
 	jwt.StandardClaims
 }
 
-const tokenExpire = int64(time.Hour * 24) //设置过期时间
+const tokenExpire = MyTime.SecondsPerDay //设置过期时间 单位s
 
 var keySecret = []byte("Go-Gin-Study123") //加盐秘钥
 
@@ -26,15 +27,16 @@ var keySecret = []byte("Go-Gin-Study123") //加盐秘钥
 func ApiLoginToken(user Model.User) (string, error) {
 	// 创建api登录声明
 	claims := ApiLoginClaims{
-		user.Name, // 自定义字段
+		// 自定义字段
+		user.ID, //用户id
 		jwt.StandardClaims{
-			Audience:  user.Name,             // 受众
-			ExpiresAt: tokenExpire,           // 失效时间
-			Id:        cvt.String(user.Uuid), // 编号
-			IssuedAt:  time.Now().Unix(),     // 签发时间
-			Issuer:    "admin",               // 签发人
-			NotBefore: time.Now().Unix(),     // 生效时间
-			Subject:   "login",               // 主题
+			Audience:  user.Name,                       // 受众
+			ExpiresAt: time.Now().Unix() + tokenExpire, // 失效时间
+			Id:        cvt.String(user.Uuid),           // 编号
+			IssuedAt:  time.Now().Unix(),               // 签发时间
+			Issuer:    "admin",                         // 签发人
+			NotBefore: time.Now().Unix(),               // 生效时间
+			Subject:   "login",                         // 主题
 		},
 	}
 	// 使用指定的签名方法创建签名对象
