d282ec3a1f44a5c2d2aa7e23a57ca520e0726622
qianjing <18651984625@163.com>
2022-08-26 09:55:13 +0800

    migration 服务启动运行数据库迁移

--- Model/user.go
+++ Model/user.go
@@ -1,6 +1,7 @@
 package Model
 
 import (
+	"fmt"
 	uuid "github.com/satori/go.uuid"
 	"go-study/Config"
 	"go-study/Library/Gorm"
@@ -8,18 +9,15 @@ import (
 	"gorm.io/gorm"
 )
 
-// 表名（未加表前缀）
-const tableName = "user"
-
 //
 // User
 // @Description: 表字段结构体
 //
 type User struct {
 	ID        uint64         `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
-	Uuid      uuid.UUID      `gorm:"column:uuid;not null;default:'';uniqueIndex:user_uuid_uindex;comment:'全局唯一标识'" json:"uuid"`
-	Name      string         `gorm:"column:name;;not null;default:'';comment:'用户名'" json:"name"`
-	Password  string         `gorm:"column:password;;not null;default:'';comment:'密码'" json:"password"`
+	Uuid      uuid.UUID      `gorm:"column:uuid;not null;default:'';uniqueIndex:user_uuid_uindex;comment:全局唯一标识" json:"uuid"`
+	Name      string         `gorm:"column:name;not null;default:'';comment:用户名" json:"name"`
+	Password  string         `gorm:"column:password;not null;default:'';comment:密码" json:"password"`
 	CreatedAt *MyTime.Time   `gorm:"column:created_at;index:user_created_at_index" json:"created_at"`
 	UpdatedAt *MyTime.Time   `gorm:"column:updated_at;index:user_updated_at_index" json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at;" json:"-"`
@@ -34,7 +32,7 @@ type User struct {
 func (User) TableName() string {
 	prefix := Config.GetFieldByName(Config.Configs.Web, "DB", "Prefix")
 
-	return prefix + tableName
+	return fmt.Sprintf("%s%s", prefix, "user")
 }
 
 // UserModel
@@ -43,4 +41,21 @@ func (User) TableName() string {
 //
 func UserModel() *gorm.DB {
 	return Gorm.Mysql.Table(User{}.TableName())
+}
+
+//
+// GetOption
+// @Description: 获取表基础配置
+// @receiver User
+// @param key
+// @return string
+//
+func (User) GetOption(key string) string {
+	option := map[string]string{
+		"engine":  "InnoDB",
+		"comment": "用户表",
+		"charset": "utf8mb4",
+	}
+
+	return option[key]
 }
