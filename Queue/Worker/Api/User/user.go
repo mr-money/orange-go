9eaf1284344ed0405ab01bd32260053e072bbfd1
qianjing <740363495@qq.com>
2022-10-14 11:25:59 +0800

    Worker目录：消费队列方法 对应App目录

--- Queue/Worker/Api/User/user.go
+++ Queue/Worker/Api/User/user.go
@@ -6,5 +6,10 @@ import (
 )
 
 func UserLog(name string) (string, error) {
-	return name, tasks.NewErrRetryTaskLater("error:", 3*time.Second)
+	//return name, tasks.NewErrRetryTaskLater("error:", 3*time.Second)
+
+	if false { //error 3秒重试
+		return name, tasks.NewErrRetryTaskLater("error:", 3*time.Second)
+	}
+	return name, nil
 }
