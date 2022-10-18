e7d4c254687f169d71b3566f2c36bf29b32e3286
qianjing <740363495@qq.com>
2022-10-18 10:01:52 +0800

    判断基本数据类型方法

--- Library/Handler/type.go
+++ Library/Handler/type.go
@@ -6,45 +6,39 @@ package Handler
 // @param items
 // @return typeList
 //
-func JudgeType(items ...interface{}) (typeList map[int]string) {
+func JudgeType(value interface{}) (typeName string) {
 
-	for index, value := range items {
-		var typeName string
-		switch value.(type) {
-		case string:
-		default:
-			typeName = "string"
-		case bool:
-			typeName = "bool"
-		case float32:
-			typeName = "float32"
-		case float64:
-			typeName = "float64"
-		case int:
-			typeName = "int"
-		case int8:
-			typeName = "int8"
-		case int16:
-			typeName = "int16"
-		case int32:
-			typeName = "int32"
-		case int64:
-			typeName = "int64"
-		case uint:
-			typeName = "uint"
-		case uint8:
-			typeName = "uint8"
-		case uint16:
-			typeName = "uint16"
-		case uint32:
-			typeName = "uint32"
-		case uint64:
-			typeName = "uint64"
-		}
-
-		typeMap := make(map[int]string)
-		typeMap[index] = typeName
+	switch value.(type) {
+	default:
+	case string:
+		typeName = "string"
+	case bool:
+		typeName = "bool"
+	case float32:
+		typeName = "float32"
+	case float64:
+		typeName = "float64"
+	case int:
+		typeName = "int"
+	case int8:
+		typeName = "int8"
+	case int16:
+		typeName = "int16"
+	case int32:
+		typeName = "int32"
+	case int64:
+		typeName = "int64"
+	case uint:
+		typeName = "uint"
+	case uint8:
+		typeName = "uint8"
+	case uint16:
+		typeName = "uint16"
+	case uint32:
+		typeName = "uint32"
+	case uint64:
+		typeName = "uint64"
 	}
 
-	return
+	return typeName
 }
