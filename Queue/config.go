b44499d073b8c430c8b70f46010e0858094c247a
Mr_Money <740363495@qq.com>
2023-03-28 16:41:01 +0800

    队列配置list

--- Queue/config.go
+++ Queue/config.go
@@ -39,3 +39,42 @@ func initConf() {
 		},*/
 	}
 }
+
+//
+// confList
+// @Description: 队列配置list
+// @return *[]config.Config
+//
+func confList() *[]config.Config {
+	return &[]config.Config{
+		{
+			Broker: fmt.Sprintf("redis://%s:%s/%s",
+				Config.GetFieldByName(Config.Configs.Web.Redis, "Host"),
+				Config.GetFieldByName(Config.Configs.Web.Redis, "Port"),
+				"1",
+			),
+			DefaultQueue: "go_study",
+			ResultBackend: fmt.Sprintf("redis://%s:%s/%s",
+				Config.GetFieldByName(Config.Configs.Web.Redis, "Host"),
+				Config.GetFieldByName(Config.Configs.Web.Redis, "Port"),
+				"1",
+			),
+			ResultsExpireIn: 3600, //结果过期时间
+			Redis: &config.RedisConfig{
+				MaxIdle:                3,
+				IdleTimeout:            240,
+				ReadTimeout:            15,
+				WriteTimeout:           15,
+				ConnectTimeout:         15,
+				NormalTasksPollPeriod:  1000,
+				DelayedTasksPollPeriod: 500,
+			},
+			/*AMQP: &config.AMQPConfig{
+				Exchange:      "go_study",
+				ExchangeType:  "direct",
+				BindingKey:    "go_study_task",
+				PrefetchCount: 3,
+			},*/
+		},
+	}
+}
