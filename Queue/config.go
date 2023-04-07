f64310d6887318d6d8b596653d80ae2cedc784d6
Mr_money <740363495@qq.com>
2023-04-07 12:52:27 +0800

    rabbitMq配置

--- Queue/config.go
+++ Queue/config.go
@@ -67,34 +67,26 @@ func confList() *[]config.Config {
 				NormalTasksPollPeriod:  1000,
 				DelayedTasksPollPeriod: 500,
 			},
-			/*AMQP: &config.AMQPConfig{
-				Exchange:      "go_study",
-				ExchangeType:  "direct",
-				BindingKey:    "go_study_task",
-				PrefetchCount: 3,
-			},*/
 		},
 		{
 			DefaultQueue: "go_study2", //队列名
-			Broker: fmt.Sprintf("redis://%s:%s/%s",
+			/*Broker: fmt.Sprintf("redis://%s:%s/%s",
 				Config.GetFieldByName(Config.Configs.Web.Redis, "Host"),
 				Config.GetFieldByName(Config.Configs.Web.Redis, "Port"),
 				"2",
-			),
+			),*/
+			Broker: "amqp://guest:guest@localhost:5672",
 			ResultBackend: fmt.Sprintf("redis://%s:%s/%s",
 				Config.GetFieldByName(Config.Configs.Web.Redis, "Host"),
 				Config.GetFieldByName(Config.Configs.Web.Redis, "Port"),
 				"2",
 			),
 			ResultsExpireIn: 3600, //结果过期时间
-			Redis: &config.RedisConfig{
-				MaxIdle:                3,
-				IdleTimeout:            240,
-				ReadTimeout:            15,
-				WriteTimeout:           15,
-				ConnectTimeout:         15,
-				NormalTasksPollPeriod:  1000,
-				DelayedTasksPollPeriod: 500,
+			AMQP: &config.AMQPConfig{
+				Exchange:      "go_study2",
+				ExchangeType:  "direct",
+				BindingKey:    "go_study_task",
+				PrefetchCount: 3,
 			},
 		},
 	}
