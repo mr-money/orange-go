de17a85972ee2057e349e298dea40981c1f671a4
钱京 <740363495@qq.com>
2023-04-11 17:03:40 +0800

    todo 关闭服务报错 close of closed channel
    Signed-off-by: 钱京 <740363495@qq.com>

--- Queue/server.go
+++ Queue/server.go
@@ -2,14 +2,12 @@ package Queue
 
 import (
 	"github.com/RichardKnop/machinery/v1"
-	"github.com/RichardKnop/machinery/v1/config"
 	"github.com/RichardKnop/machinery/v1/tasks"
 	"go-study/Library/Handler"
 	"log"
 )
 
-//var server *machinery.Server
-var serverMap map[string]*machinery.Server
+var server *machinery.Server
 
 func Run() {
 	/*rootPath, _ := os.Getwd()
@@ -20,7 +18,7 @@ func Run() {
 	}*/
 
 	//初始化队列&消费任务
-	initTasks()
+	/*initTasks()
 
 	//循环创建队列server
 	serverMap = make(map[string]*machinery.Server)
@@ -50,6 +48,39 @@ func Run() {
 
 			serverMap[confIn.DefaultQueue] = server
 		}(conf)
+	}*/
+
+	var err error
+	//创建队列server
+	conf := initConf()
+	server, err = machinery.NewServer(conf)
+	if err != nil {
+		log.Println("start server failed", err)
+		return
+	}
+
+	//循环队列&创建监听消费
+	for _, queue := range *getQueues() {
+		func(queueIn queueGroups) {
+			//worker := server.NewWorker(queueIn.queueName, 1)
+			worker := server.NewCustomQueueWorker(queueIn.queueName, 1, queueIn.queueName)
+
+			go func(workerIn *machinery.Worker, queueName string) {
+				err = workerIn.Launch()
+				if err != nil {
+					log.Println("start "+queueName+": worker error", err)
+					return
+				}
+
+			}(worker, queueIn.queueName)
+
+			//注册任务
+			err = server.RegisterTasks(queueIn.tasks)
+			if err != nil {
+				log.Panicln("register tasks in queue: "+queueIn.queueName+" failed", err)
+			}
+		}(queue)
+
 	}
 
 }
@@ -84,18 +115,18 @@ func AddTask(taskName string, params map[string]interface{}) string {
 	}
 
 	//发送任务
-	server := serverMap[queueName]
+	server := server
 	asyncResult, err := server.SendTask(signature)
 	if err != nil {
 		log.Panicln(err)
 	}
 
 	//获取结果
-	res, err := asyncResult.Get(1)
+	/*res, err := asyncResult.Get(1)
 	if err != nil {
 		log.Panicln(err)
 	}
-	log.Printf("queue get res is %v\n", tasks.HumanReadableResults(res))
+	log.Printf("queue get res is %v\n", tasks.HumanReadableResults(res))*/
 	//return tasks.HumanReadableResults(res)
 	return asyncResult.GetState().State
 }
