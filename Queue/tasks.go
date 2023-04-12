8413f9dd79aa5438ef01f3024996f92cb99e6b67
钱京 <740363495@qq.com>
2023-04-12 10:20:16 +0800

    server使用map对应不同队列queue
    Signed-off-by: 钱京 <740363495@qq.com>

--- Queue/tasks.go
+++ Queue/tasks.go
@@ -5,30 +5,11 @@ import (
 	"go-study/Queue/Worker/Api/QueueDemo"
 )
 
+// 定义队列消费方法名称
 const (
 	PrintNameFunc  = "print_name"
 	PrintName2Func = "print_name2"
 )
-
-// 任务列表
-//var tasksList = map[string]map[string]interface{}{}
-
-//
-// initTasks
-// @Description: 配置队列及相关消费方法
-//
-/*func initTasks() {
-	for _, conf := range *confList() {
-		tasksList[conf.DefaultQueue] = make(map[string]interface{})
-		switch conf.DefaultQueue {
-		//区分不同队列任务
-		case "go_study":
-			tasksList[conf.DefaultQueue][PrintNameFunc] = QueueDemo.PrintName
-		case "go_study2":
-			tasksList[conf.DefaultQueue][PrintName2Func] = QueueDemo.PrintName2
-		}
-	}
-}*/
 
 //
 // getQueueByTask
@@ -38,12 +19,6 @@ const (
 // @return error
 //
 func getQueueByTask(taskName string) (string, error) {
-	/*for queue, tasks := range tasksList {
-		if _, ok := tasks[taskName]; ok {
-			return queue, nil
-		}
-	}*/
-
 	for _, queue := range *getQueues() {
 		if _, ok := queue.tasks[taskName]; ok {
 			return queue.queueName, nil
@@ -53,7 +28,7 @@ func getQueueByTask(taskName string) (string, error) {
 	return "", errors.New("任务：" + taskName + " 队列不存在")
 }
 
-//队列组配置
+//队列组
 type queueGroups struct {
 	queueName string                 //队列名称
 	tasks     map[string]interface{} //队列下任务 任务名称：任务消费方法
