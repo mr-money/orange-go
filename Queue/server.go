cfb40d3fca0e6d218dd9218fe30f413953c93910
Mr_Money <740363495@qq.com>
2023-04-12 10:20:16 +0800

    server使用map对应不同队列queue
    Signed-off-by: 钱京 <740363495@qq.com>

--- Queue/server.go
+++ Queue/server.go
@@ -7,7 +7,8 @@ import (
 	"log"
 )
 
-var server *machinery.Server
+// server对应不同队列queue
+var serverMap map[string]*machinery.Server
 
 func Run() {
 	/*rootPath, _ := os.Getwd()
@@ -17,54 +18,29 @@ func Run() {
 		return
 	}*/
 
-	//初始化队列&消费任务
-	/*initTasks()
-
-	//循环创建队列server
-	serverMap = make(map[string]*machinery.Server)
-	for _, conf := range *confList() {
-		func(confIn config.Config) {
-			server, err := machinery.NewServer(&confIn)
-			if err != nil {
-				log.Println("start server failed", err)
-				return
-			}
-
-			worker := server.NewWorker(confIn.DefaultQueue, 1)
-			go func(workerIn *machinery.Worker) {
-				err = workerIn.Launch()
-				if err != nil {
-					log.Println("start "+confIn.DefaultQueue+": worker error", err)
-					return
-				}
-
-			}(worker)
-
-			//注册任务
-			err = server.RegisterTasks(tasksList[confIn.DefaultQueue])
-			if err != nil {
-				log.Panicln("register tasks in queue: "+confIn.DefaultQueue+" failed", err)
-			}
-
-			serverMap[confIn.DefaultQueue] = server
-		}(conf)
-	}*/
-
 	var err error
-	//创建队列server
+
+	//初始化队列配置
 	conf := initConf()
-	server, err = machinery.NewServer(conf)
-	if err != nil {
-		log.Println("start server failed", err)
-		return
-	}
+
+	//server需要使用map对应不同队列queue
+	serverMap = make(map[string]*machinery.Server)
 
 	//循环队列&创建监听消费
 	for _, queue := range *getQueues() {
+		serverMap[queue.queueName], err = machinery.NewServer(conf)
+		if err != nil {
+			log.Println("start server failed", err)
+			return
+		}
+
+		//匿名闭包使用队列组
 		func(queueIn queueGroups) {
 			//worker := server.NewWorker(queueIn.queueName, 1)
-			worker := server.NewCustomQueueWorker(queueIn.queueName, 1, queueIn.queueName)
+			//创建custom queue
+			worker := serverMap[queueIn.queueName].NewCustomQueueWorker(queueIn.queueName, 1, queueIn.queueName)
 
+			//启动goroutine运行worker
 			go func(workerIn *machinery.Worker, queueName string) {
 				err = workerIn.Launch()
 				if err != nil {
@@ -75,7 +51,7 @@ func Run() {
 			}(worker, queueIn.queueName)
 
 			//注册任务
-			err = server.RegisterTasks(queueIn.tasks)
+			err = serverMap[queueIn.queueName].RegisterTasks(queueIn.tasks)
 			if err != nil {
 				log.Panicln("register tasks in queue: "+queueIn.queueName+" failed", err)
 			}
@@ -115,7 +91,7 @@ func AddTask(taskName string, params map[string]interface{}) string {
 	}
 
 	//发送任务
-	server := server
+	server := serverMap[queueName]
 	asyncResult, err := server.SendTask(signature)
 	if err != nil {
 		log.Panicln(err)
@@ -128,5 +104,6 @@ func AddTask(taskName string, params map[string]interface{}) string {
 	}
 	log.Printf("queue get res is %v\n", tasks.HumanReadableResults(res))*/
 	//return tasks.HumanReadableResults(res)
+
 	return asyncResult.GetState().State
 }
