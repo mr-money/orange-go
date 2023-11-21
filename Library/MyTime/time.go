59651299606fae86050d3f8e8213feca611d2a1b
Mr_Money <740363495@qq.com>
2023-11-21 12:00:50 +0800

    时间字符串转Time
    Signed-off-by: 钱京 <740363495@qq.com>

--- Library/MyTime/time.go
+++ Library/MyTime/time.go
@@ -49,7 +49,7 @@ func (t Time) Value() (driver.Value, error) {
 // Scan 检出 mysql 时调用
 func (t *Time) Scan(v interface{}) error {
 	// mysql 内部日期的格式可能是 2006-01-02 15:04:05 +0800 CST 格式，所以检出的时候还需要进行一次格式化
-	tTime, _ := time.Parse("2006-01-02 15:04:05 +0800 CST", v.(time.Time).String())
+	tTime, _ := time.Parse(TimeFormat+" +0800 CST", v.(time.Time).String())
 	*t = Time(tTime)
 	return nil
 }
@@ -57,4 +57,19 @@ func (t *Time) Scan(v interface{}) error {
 // 用于 fmt.Println 和后续验证场景
 func (t Time) String() string {
 	return time.Time(t).Format(TimeFormat)
+}
+
+//
+// StrToTime
+// @Description: 时间字符串转Time
+// @param value
+// @return Time
+//
+func StrToTime(value string) Time {
+	t, err := time.Parse(TimeFormat, value)
+	if err != nil {
+		return Time{}
+	}
+
+	return Time(t)
 }
