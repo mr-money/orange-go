9ff0d0453bf0e0aca1e6e9bcca25bef79b724d15
mr_money <740363495@qq.com>
2024-10-25 10:52:25 +0800

    跨域中间件
    Signed-off-by: mr_money <740363495@qq.com>

--- MiddleWare/cors.go
+++ MiddleWare/cors.go
@@ -5,16 +5,13 @@ import (
 	"net/http"
 )
 
-//
-// Cors
 // @Description: 跨域中间件
 // @return gin.HandlerFunc
-//
 func Cors() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		method := context.Request.Method
 		context.Header("Access-Control-Allow-Origin", "*")
-		context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token, X-Requested-With")
 		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 		context.Header("Access-Control-Allow-Credentials", "true")
