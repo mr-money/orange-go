5f981d4f74ba7c19911658b7232697204b2e7623
mr_money <740363495@qq.com>
2024-10-25 11:00:21 +0800

    类型判空
    Signed-off-by: mr_money <740363495@qq.com>

--- Library/Handler/type.go
+++ Library/Handler/type.go
@@ -1,5 +1,10 @@
 package Handler
 
+import (
+	"github.com/pkg/errors"
+	"reflect"
+)
+
 //
 // JudgeType
 // @Description: 判断基本数据类型
@@ -41,4 +46,56 @@ func JudgeType(value interface{}) (typeName string) {
 	}
 
 	return typeName
+}
+
+//
+// IsEmpty
+// @Description: 判断数据是否为空
+// @param data
+// @return error
+//
+func IsEmpty(data interface{}) error {
+	if data == nil {
+		return errors.New("Data is nil")
+	}
+
+	value := reflect.ValueOf(data)
+	switch value.Kind() {
+	case reflect.String:
+		if value.String() == "" {
+			return errors.New("String is empty")
+		}
+	case reflect.Slice, reflect.Array, reflect.Map:
+		if value.Len() == 0 {
+			return errors.New("Slice, Array, or Map is empty")
+		}
+	case reflect.Ptr:
+		if value.IsNil() {
+			return errors.New("Pointer is nil")
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		if value.Int() == 0 {
+			return errors.New("Int is empty")
+		}
+	case reflect.Float32, reflect.Float64:
+		if value.Float() == 0.0 {
+			return errors.New("Float is empty")
+		}
+	case reflect.Bool:
+		if !value.Bool() {
+			return errors.New("Bool is false")
+		}
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		if value.Uint() == 0 {
+			return errors.New("Uint is empty")
+		}
+	case reflect.Struct:
+		// Check for an empty struct
+		zeroValue := reflect.Zero(value.Type())
+		if value.Interface() == zeroValue.Interface() {
+			return errors.New("Struct is empty")
+		}
+	}
+
+	return nil
 }
