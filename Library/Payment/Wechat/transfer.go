6d94d1cacdf8ed454411a578b8a93d2d7624af51
Mr_Money <740363495@qq.com>
2024-10-25 12:53:00 +0800

    微信支付相关
    Signed-off-by: mr_money <740363495@qq.com>

--- Library/Payment/Wechat/transfer.go
+++ Library/Payment/Wechat/transfer.go
@@ -2,52 +2,130 @@ package Wechat
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/wechatpay-apiv3/wechatpay-go/core"
-	"github.com/wechatpay-apiv3/wechatpay-go/services/transferbatch"
+	"github.com/wechatpay-apiv3/wechatpay-go/services/partnertransferbatch"
+	"io/ioutil"
+	"log"
 )
 
 //
-// ServiceTransfer
-// @Description: 商家转账
-// @receiver conf
-// @return interface{}
+// TransferBatch
+// @Description: 批量转账数据
+//
+type TransferBatch struct {
+	OutBatchNo  string //商家批次单号 商户系统内部唯一
+	BatchName   string //批次名称
+	BatchRemark string //批次备注
+	TotalAmount int64  //转账总金额 分
+	TotalNum    int64  //转账总笔数
+	DetailList  []struct {
+		Openid         string
+		OutDetailNo    string //商家明细单号
+		TransferAmount int64  //	转账金额 分
+		TransferRemark string // 转账备注 最多允许32个字符
+		UserName       string //收款用户姓名
+		//UserIdCard     string //收款用户身份证号
+	}
+}
+
+//
+// TransferBatch
+// @Description: 发起商家转账 商户可以通过该接口同时向多个用户微信零钱进行转账操作
+// @receiver conf 支付配置
+// @param TransferBatch 提现数据
+// @return interface{} 微信返回
 // @return error
 //
-func (conf WxConf) ServiceTransfer() (interface{}, error) {
+func (conf WxConf) TransferBatch(TransferBatch *TransferBatch) (interface{}, error) {
 	wechatClient, err := clientInit(conf)
 	if err != nil {
 		return nil, err
 	}
 
-	svc := transferbatch.TransferBatchApiService{
-		Client: wechatClient,
+	//服务商转账请求数据
+	transferBatchRequest := partnertransferbatch.InitiateTransferBatchRequest{
+		SubMchid:          core.String(conf.SubMchid),
+		SubAppid:          core.String(conf.SubAppid),
+		AuthorizationType: partnertransferbatch.AUTHTYPE_INFORMATION_AUTHORIZATION_TYPE.Ptr(),
+		OutBatchNo:        core.String(TransferBatch.OutBatchNo),
+		BatchName:         core.String(TransferBatch.BatchName),
+		BatchRemark:       core.String(TransferBatch.BatchRemark),
+		TotalAmount:       core.Int64(TransferBatch.TotalAmount),
+		TotalNum:          core.Int64(TransferBatch.TotalNum),
+		SpAppid:           core.String(conf.SpAppid),
+		TransferPurpose:   partnertransferbatch.TRANSFERUSETYPE_GOODSPAYMENT.Ptr(),
+		TransferScene:     partnertransferbatch.TRANSFERSCENE_ORDINARY_TRANSFER.Ptr(),
 	}
 
-	resp, result, err := svc.InitiateBatchTransfer(
+	var TransferDetailList []partnertransferbatch.TransferDetailInput
+	for _, detail := range TransferBatch.DetailList {
+		TransferDetailList = append(TransferDetailList, partnertransferbatch.TransferDetailInput{
+			Openid:         core.String(detail.Openid),
+			OutDetailNo:    core.String(detail.OutDetailNo),
+			TransferAmount: core.Int64(detail.TransferAmount),
+			TransferRemark: core.String(detail.TransferRemark),
+			UserName:       core.String(detail.UserName),
+			//UserIdCard:     core.String(detail.UserIdCard),
+		})
+	}
+
+	transferBatchRequest.TransferDetailList = TransferDetailList
+
+	fmt.Printf("transferBatchRequest---------%v\n", transferBatchRequest)
+
+	svc := partnertransferbatch.TransferBatchApiService{Client: wechatClient}
+
+	resp, apiResult, err := svc.InitiateTransferBatch(
 		context.Background(),
-		transferbatch.InitiateBatchTransferRequest{
-			Appid:       core.String("wxf636efh567hg4356"),
-			OutBatchNo:  core.String("plfk2020042013"),
-			BatchName:   core.String("2019年1月深圳分部报销单"),
-			BatchRemark: core.String("2019年1月深圳分部报销单"),
-			TotalAmount: core.Int64(4000000),
-			TotalNum:    core.Int64(200),
-			TransferDetailList: []transferbatch.TransferDetailInput{transferbatch.TransferDetailInput{
-				OutDetailNo:    core.String("x23zy545Bd5436"),
-				TransferAmount: core.Int64(200000),
-				TransferRemark: core.String("2020年4月报销"),
-				Openid:         core.String("o-MYE42l80oelYMDE34nYD456Xoy"),
-				UserName:       core.String("757b340b45ebef5467rter35gf464344v3542sdf4t6re4tb4f54ty45t4yyry45"),
-			}},
-			TransferSceneId: core.String("1000"),
-		},
+		transferBatchRequest,
 	)
 
-	fmt.Println("==========resp==========")
-	fmt.Println(resp)
-	fmt.Println("==========result==========")
-	fmt.Println(result)
+	//商家转账请求数据
+	/*transferBatchRequest := transferbatch.InitiateBatchTransferRequest{
+		Appid:           core.String(conf.SubAppid),
+		OutBatchNo:      core.String(TransferBatch.OutBatchNo),
+		BatchName:       core.String(TransferBatch.BatchName),
+		BatchRemark:     core.String(TransferBatch.BatchRemark),
+		TotalAmount:     core.Int64(TransferBatch.TotalAmount),
+		TotalNum:        core.Int64(TransferBatch.TotalNum),
+		TransferSceneId: core.String("1001"),
+	}
 
-	return resp, err
+	var TransferDetailList []transferbatch.TransferDetailInput
+	for _, detail := range TransferBatch.DetailList {
+		TransferDetailList = append(TransferDetailList, transferbatch.TransferDetailInput{
+			Openid:         core.String(detail.Openid),
+			OutDetailNo:    core.String(detail.OutDetailNo),
+			TransferAmount: core.Int64(detail.TransferAmount),
+			TransferRemark: core.String(detail.TransferRemark),
+			UserName:       core.String(detail.UserName),
+		})
+	}
+	transferBatchRequest.TransferDetailList = TransferDetailList
+
+	fmt.Printf("transferBatchRequest---------%v\n", transferBatchRequest)
+
+	svc := transferbatch.TransferBatchApiService{Client: wechatClient}
+
+	resp, apiResult, err := svc.InitiateBatchTransfer(
+		context.Background(),
+		transferBatchRequest,
+	)*/
+
+	if err != nil {
+		// 处理错误
+		log.Printf("call InitiateBatchTransfer err:%s", err)
+	} else {
+		// 处理返回结果
+		log.Printf("status=%d resp=%s", apiResult.Response.StatusCode, resp)
+	}
+
+	transferRes, _ := ioutil.ReadAll(apiResult.Response.Body)
+
+	var result map[string]interface{}
+	_ = json.Unmarshal(transferRes, &result)
+
+	return transferRes, nil
 }
---
+++ Library/Wechat/login.go
@@ -0,0 +1,62 @@
+package Wechat
+
+import (
+	"crypto/tls"
+	"github.com/pkg/errors"
+	"net/http"
+	"orange-go/Library/Handler"
+	"strings"
+)
+
+// Code2SessionResponse
+// @Description: 小程序登录返回
+type Code2SessionResponse struct {
+	SessionKey string `json:"session_key"`
+	Unionid    string `json:"unionid"`
+	Errmsg     string `json:"errmsg"`
+	Openid     string `json:"openid"`
+	Errcode    int    `json:"errcode"`
+}
+
+// Code2Session
+// @Description:小程序登录
+// @param code前端获取code
+// @return interface{}
+// @return error
+func Code2Session(code string) (Code2SessionResponse, error) {
+	//小程序配置
+	config := GetWxappConfig()
+
+	client := resty.New()
+
+	url := "https://api.weixin.qq.com/sns/jscode2session"
+	if strings.HasPrefix(url, "https://") {
+		verify := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client.SetTransport(verify)
+	}
+
+	//发送请求
+	res, err := client.R().
+		SetQueryParams(map[string]string{
+			"appid":      config.Appid,
+			"secret":     config.Appsecret,
+			"js_code":    code,
+			"grant_type": "authorization_code",
+		}).
+		Get(url)
+
+	if err != nil {
+		return Code2SessionResponse{}, err
+	}
+
+	var response Code2SessionResponse
+	Handler.JsonToStruct(res.String(), &response)
+
+	if response.Errcode != 0 {
+		return response, errors.New(response.Errmsg)
+	}
+
+	return response, nil
+}
